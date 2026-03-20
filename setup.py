1ad262b4bed0ccc2cfd0edf461d4d4a0ddc41e34
kamalgrok06-droid <kamalgrok06@gmail.com>
2026-03-20 16:37:46 +0545

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="image-text-embedder",
+    version="1.0.0",
+    author="Image Text Embedder",
+    description="Batch embed customizable text overlays on images",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["Pillow>=10.0.0"],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "image-text-embedder=ImageTextEmbedder:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/YOUR_USERNAME/Image-Text-Embedder",
+)
